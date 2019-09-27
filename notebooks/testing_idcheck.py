20834d43f973722e79662c9b358ff124978c5879
Fernando D. Hernandez S <fernandohdz915@hotmail.com>
2019-09-27 18:03:02 -0500

    eliminating lint errors  and warnings from testing_idcheck.py

--- notebooks/testing_idcheck.py
+++ notebooks/testing_idcheck.py
@@ -1,8 +1,5 @@
-from check_id import web_search 
-import pandas as pd  
-import numpy as np
-from bs4 import BeautifulSoup
-import requests
+from check_id import web_search
+import pandas as pd
 
 ine_data = pd.read_csv('list_id_data.csv')
 
@@ -13,17 +10,17 @@ test_data['emision'] = '0{}'.format(str(test_data.loc['emision']))
 test_data
 
 if test_data['tipo_cred'] == 'a':
-  test_data['ocr_vertical'] = str(int(test_data.loc['ocr_vertical']))
+    test_data['ocr_vertical'] = str(int(test_data.loc['ocr_vertical']))
 elif test_data['tipo_cred'] == 'd':
-  test_data['ocr_horizontal'] = str(int(test_data.loc['ocr_horizontal']))
-  test_data['cic'] = str(int(test_data.loc['cic']))
+    test_data['ocr_horizontal'] = str(int(test_data.loc['ocr_horizontal']))
+    test_data['cic'] = str(int(test_data.loc['cic']))
 elif test_data['tipo_cred'] == 'e':
-  test_data['cve_ciudadano'] = str(int(test_data.loc['cve_ciudadano']))
-  test_data['cic'] = str(int(test_data.loc['cic']))
+    test_data['cve_ciudadano'] = str(int(test_data.loc['cve_ciudadano']))
+    test_data['cic'] = str(int(test_data.loc['cic']))
 
 
 id_check = web_search.consulta_id(test_data)
 
-res = id_check.ine_check(api_key = 'f93c8b9646c63020ef084ecac088583d')
+res = id_check.ine_check(api_key='f93c8b9646c63020ef084ecac088583d')
 
 print(res)
