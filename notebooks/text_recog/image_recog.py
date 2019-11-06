1d55678397330a74cb341b405ae07836b82c1a53
Fernando D. Hernandez S <fernandohdz915@hotmail.com>
2019-11-05 22:31:03 -0600

    adding new algorithm for croping and detection

--- notebooks/text_recog/image_recog.py
+++ notebooks/text_recog/image_recog.py
@@ -3,42 +3,40 @@ import pytesseract
 import argparse
 import cv2
 import os
+import imutils
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-	help="path to input image to be OCR'd")
-ap.add_argument("-p", "--preprocess", type=str, default="thresh",
-	help="type of preprocessing to be done")
-args = vars(ap.parse_args())
+class image_recognition():
 
-# load the example image and convert it to grayscale
-image = cv2.imread(args["image"])
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# check to see if we should apply thresholding to preprocess the
-# image
-if args["preprocess"] == "thresh":
-	gray = cv2.threshold(gray, 0, 255,
-		cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
- 
-# make a check to see if median blurring should be done to remove
-# noise
-elif args["preprocess"] == "blur":
-	gray = cv2.medianBlur(gray, 3)
- 
-# write the grayscale image to disk as a temporary file so we can
-# apply OCR to it
-filename = "{}.png".format(os.getpid())
-cv2.imwrite(filename, gray)
+  def __init__(self, img_path, preprocess):
+    self.img_path = str(img_path)
+    self.preprocess = str(preprocess)
 
-# load the image as a PIL/Pillow image, apply OCR, and then delete
-# the temporary file
-text = pytesseract.image_to_string(Image.open(filename))
-os.remove(filename)
-print(text)
- 
-# show the output images
-#cv2.imshow("Image", image)
-#cv2.imshow("Output", gray)
-cv2.waitKey(0)
+
+  def img_recog(self):
+      #load the example image and convert it to grayscale
+      image = cv2.imread(self.img_path)
+      image = imutils.resize(image, height=600)
+
+      gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+      # check to see if we should apply thresholding to preprocess the
+      # image
+      if self.preprocess == "thresh":
+          gray = cv2.threshold(gray, 0, 255,
+          cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+      # make a check to see if median blurring should be done to remove
+      # noise
+      elif self.preprocess == "blur":
+      	  gray = cv2.medianBlur(gray, 3)
+      # write the grayscale image to disk as a temporary file so we can
+      # apply OCR to it
+      filename = "{}.png".format(os.getpid())
+      cv2.imwrite(filename, gray)
+      # load the image as a PIL/Pillow image, apply OCR, and then delete
+      # the temporary file
+      text = pytesseract.image_to_string(Image.open(filename), lang='spa')
+      info = pytesseract.image_to_data(Image.open(filename), lang='spa')
+      os.remove(filename)
+      output = [text, info]
+      return(output)
+
