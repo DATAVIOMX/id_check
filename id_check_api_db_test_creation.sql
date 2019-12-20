4e319bf2fabdc192db9ef77b8f632c19c51f8551
Otto Hahn Herrera <ottohahn@users.noreply.github.com>
2019-12-20 13:54:45 -0600

    Creation of tables for testing database

---
+++ id_check_api_db_test_creation.sql
@@ -0,0 +1,22 @@
+CREATE TABLE users(
+    userid INTEGER AUTOINCREMENT PRIMARY KEY,
+    username TEXT,
+    pwhash TEXT,
+    creation-date TEXT,
+    update-date TEXT,
+    status INT,
+    api-key TEXT,
+    api-key-exp-date TEXT
+    );
+    
+CREATE TABLE api-calls(
+    callid INTEGER AUTOINCREMENT PRIMARY KEY
+    userid INTEGER FOREIGN KEY REFERENCES users(userid),
+    api-key TEXT,
+    call-date TEXT,
+    call-point TEXT,
+    status code INT,
+    call-text TEXT
+);
+
+
