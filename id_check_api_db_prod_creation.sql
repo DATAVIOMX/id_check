ea1f7482466f9051646d909a0644ca79021544c6
ottohahn <otto.hahn.herrera@gmail.com>
2020-01-24 01:00:11 -0600

    secure table definitions

--- id_check_api_db_prod_creation.sql
+++ id_check_api_db_prod_creation.sql
@@ -4,26 +4,31 @@ UUIDs and the format for the dates.
 AUTHOR: Otto Hahn
 */
 
+/* Extensions */
+create extension pgcrypto;
+create extension chkpass;
 
-CREATE TABLE public.users (
-    userid uuid NOT NULL,
+/* Table definitions */
+
+CREATE TABLE users (
+    userid uuid primary key,
     creation_date timestamp without time zone,
     update_date timestamp without time zone,
     status integer,
-    api_key text,
+    api_key chkpass,
     api_key_exp_date timestamp without time zone,
     calls_remaining integer
 );
 
-CREATE TABLE public.api_calls (
+CREATE TABLE api_calls (
     callid integer NOT NULL,
-    userid uuid,
-    api_key text,
+    userid uuid references users(userid),
     call_date timestamp without time zone,
     call_point text,
     status_code text,
-    call_text json,
-    response json
+    call_text bytea,
+    response bytea
+    
 );
 
 
