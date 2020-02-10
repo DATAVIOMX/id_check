239acd70fffd59167a06a20e3627e53ceb202f8c
Otto Hahn Herrera <otto.hahn.herrera@gmail.com>
2020-02-10 14:06:54 -0600

    simplifying checking procedure

---
+++ api/id_check.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+check_id is a module that holds functions to check an INE id either from the
+image or the data fields 
+"""
+import cv2
+import pytesseract
+import numpy
+
+def check_images(front, back):
+    
+
+    
+    response = check_text()
+    return response
+
+def check_text():
+    # Something
+    return response
+
+def check_qr():
+    # Something
+    return None
+
+def check_w_api():
+
+    return None
+
+def 
+
+    
+
+
