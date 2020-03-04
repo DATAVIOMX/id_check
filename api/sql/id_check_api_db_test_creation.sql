3cddf77b0bbf8461358ea9e184c2c13b484c4532
Otto Hahn Herrera <otto.hahn.herrera@gmail.com>
2020-03-04 15:16:06 -0600

    organized files

---
+++ api/sql/id_check_api_db_test_creation.sql
@@ -0,0 +1,21 @@
+CREATE TABLE users(
+    userid INTEGER AUTO INCREMENT PRIMARY KEY,
+    creation_date TEXT,
+    update_date TEXT,
+    status INT,
+    api_key TEXT,
+    api_key_exp_date TEXT,
+    calls_remaining INTEGER
+    );
+    
+CREATE TABLE api_calls(
+    callid INTEGER AUTO INCREMENT PRIMARY KEY,
+    userid INTEGER,
+    api_key TEXT,
+    call_date TEXT,
+    call_point TEXT,
+    status_code INT,
+    call_text TEXT,
+    response TEXT,
+    FOREIGN KEY(userid) REFERENCES users(userid)
+);
---
+++ api/sql/id_check_api_db_test_testdata.sql
@@ -0,0 +1,15 @@
+/* 
+DATA INSERTION FOR TESTING THE DATABASE
+AUTHOR: MOTOKO RESEARCH
+DATE: 2019-12-20
+*/
+
+
+/* Data insertion */
+
+INSERT INTO users(userid, creation_date, update_date, status, api_key, api_key_exp_date, calls_remaining) VALUES (1, '2019-12-21T12:00:00.0000','2019-12-21T12:00:00.0000', 0, 'oBE3PSTPOswJDRIfGCbhRnxE00rgGNNW','2020-01-21T12:00:00.0000',1000);
+INSERT INTO users(userid, creation_date, update_date, status, api_key, api_key_exp_date, calls_remaining) VALUES (2, '2019-12-21T12:00:00.0000','2019-12-21T12:00:00.0000', 1, 'B7AX0KEmUXUwhKO8MjBxFw6VJ-PzNMCp','2020-01-21T12:00:00.0000',1000);
+INSERT INTO users(userid, creation_date, update_date, status, api_key, api_key_exp_date, calls_remaining) VALUES (3, '2019-12-21T12:00:00.0000','2019-12-21T12:00:00.0000', 2, 'OEU6muXB4fGCtTWc1CU_9O8bf9FLpCzO','2020-01-21T12:00:00.0000',1000);
+INSERT INTO users(userid, creation_date, update_date, status, api_key, api_key_exp_date, calls_remaining) VALUES (4, '2019-12-21T12:00:00.0000','2019-12-21T12:00:00.0000', 0, 'jqYGUkI9bHfdGVhQAS6_zqnLwlY3M1uI','2020-01-21T12:00:00.0000',1000);
+INSERT INTO users(userid, creation_date, update_date, status, api_key, api_key_exp_date, calls_remaining) VALUES (5, '2019-12-21T12:00:00.0000','2019-12-21T12:00:00.0000', 0, '_LbQU-0KWafYh5Krgco0CvBq03_n7U3d','2019-01-01T12:00:00.0000',1000);
+INSERT INTO users(userid, creation_date, update_date, status, api_key, api_key_exp_date, calls_remaining) VALUES (6, '2019-12-21T12:00:00.0000','2019-12-21T12:00:00.0000', 0, 'MJfvgk91xkTdRyDNPc-TO1LhxK6fo_4a','2020-01-21T12:00:00.0000',0);
