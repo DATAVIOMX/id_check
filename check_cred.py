a124b866973213c1bf8bfcee0d00fdeabcc7285b
Fernando D. Hernandez S <fernandohdz915@hotmail.com>
2020-08-21 17:29:32 -0500

    finetuning for demo

--- check_cred.py
+++ check_cred.py
@@ -1,29 +1,28 @@
 from scripts.complete_validation.comp_process import id_all_flow as comp_flow
 import argparse 
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--front", help="Path to the front image.")
+parser.add_argument("-b", "--back", help="Path to the back image.")
+
+args = parser.parse_args()
 
 def main():
-    parser = argparse.ArgumentParser()
-    
-    parser.add_argument("-f", "--front", help="Path to the front image.")
-    parser.add_argument("-b", "--back", help="Path to the back image.")
-
-    args = parser.parse_args()
 
     comp_prcss = comp_flow(front_img_path=args.front,
                             back_img_path=args.back,
-                            f_heigths = [400,600],
-                            b_heigths = [400,600])
+                            f_heigths = [200, 400,700,900],
+                            b_heigths = [200, 400,700,900])
     _, validation = comp_prcss.id_wrapper()
     
-    if len(validation[1] > 0):
+    if len(validation[1]) > 0:
         with open('results/result.html', 'w') as filename:
             filename.write(str(validation[0]))
-        print(len(validation[1]))
+        print(validation[1])
     
-        print(len(validation[2]))
+  
+    else:
+        print('NOT RESULT')
     
-    
-
 if __name__ == '__main__':
     main()
