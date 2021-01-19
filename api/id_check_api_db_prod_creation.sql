adec71ab4b89f13113f28397fc0939b0479c8d98
ottohahn <otto.hahn.herrera@gmail.com>
2021-01-19 15:39:38 -0600

    Removed chkpass extension references

---
+++ api/id_check_api_db_prod_creation.sql
@@ -0,0 +1,31 @@
+/*
+PURPOSE: Tables for production in PostgreSQL this changes the integer IDs for UUIDs and the format for the dates.
+AUTHOR: Otto Hahn
+*/
+
+/* Extensions */
+create extension pgcrypto;
+-- create extension chkpass;
+
+/* Table definitions */
+
+CREATE TABLE users (
+    userid uuid primary key,
+    creation_date timestamp without time zone,
+    update_date timestamp without time zone,
+    status integer,
+    api_key text,
+    api_key_exp_date timestamp without time zone,
+    calls_remaining integer
+);
+
+CREATE TABLE api_calls (
+    callid serial primary key,
+    userid uuid references users(userid),
+    call_date timestamp without time zone,
+    call_point text,
+    status_code text,
+    call_text bytea,
+    response bytea
+);
+
